ac048409b9694e6a576fbc73254a7fb7c88b5013
Mo'men <mmm.isam.99@gmail.com>
2025-11-12 13:33:00 +0200

    the test version

--- app/data/technologies.ts
+++ app/data/technologies.ts
@@ -1,6 +1,3 @@
-import html from "@/public/assets/html.png";
-import css from "@/public/assets/css.png";
-import javascript from "@/public/assets/javascript.png";
 import typescript from "@/public/assets/typescript.png";
 import reactjs from "@/public/assets/react.png";
 import redux from "@/public/assets/redux.png";
@@ -15,6 +12,9 @@ import shadcn from "@/public/assets/shadcn.png";
 import zod from "@/public/assets/zod.png";
 import react_hook_form from "@/public/assets/reac-hook-form.png";
 import react_router from "@/public/assets/react-router.png";
+import html from "@/public/assets/html.png";
+import css from "@/public/assets/css.png";
+import javascript from "@/public/assets/javascript.png";
 
 export const technologies = [
   {
@@ -53,10 +53,10 @@ export const technologies = [
     name: "PostgreSQL",
     icon: postgresql.src,
   },
-  {
-    name: "Git",
-    icon: git.src,
-  },
+  // {
+  //   name: "Git",
+  //   icon: git.src,
+  // },
   {
     name: "GitHub",
     icon: github.src,
@@ -69,10 +69,10 @@ export const technologies = [
     name: "MUI",
     icon: MUI.src,
   },
-  {
-    name: "Shadsn/ui",
-    icon: shadcn.src,
-  },
+  // {
+  //   name: "Shadsn/ui",
+  //   icon: shadcn.src,
+  // },
   {
     name: "Zod validation",
     icon: zod.src,
@@ -81,8 +81,8 @@ export const technologies = [
     name: "React hook form",
     icon: react_hook_form.src,
   },
-  {
-    name: "React router dom",
-    icon: react_router.src,
-  },
+  // {
+  //   name: "React router dom",
+  //   icon: react_router.src,
+  // },
 ];
